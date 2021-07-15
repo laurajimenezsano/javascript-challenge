0cfcee1d84c6a578096684406035b8365bcdd524
Laura Jimenez <laurajimenez95@gmail.com>
2021-07-14 23:25:22 -0400

    final

--- static/app.js
+++ static/app.js
@@ -7,61 +7,53 @@ var tableData = data;
 
 var tbody = d3.select("tbody");
 
-console.log(tableData);
+// Function to create table
 
-tableData.forEach((ufo_info) => {
-    var row = tbody.append("tr");
-    Object.entries(ufo_info).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
+function createTable(tableData){
+      // Clear existing
+      tbody.html("");
+      // loop and append
+      tableData.forEach((ufo_info) => {
+        var row = tbody.append("tr");
+        Object.entries(ufo_info).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
     });
-  });
+  })
+}
+ 
+  // COMMAND TO FILTER
+
+
+// Getting a reference to the button on the page with the id property set to `click-me`
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#filters");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
   
-// // filter data based on buttom input
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+  
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
 
-// var inputElement = d3.select("#datetime");
-// var inputValue = inputElement.property("value");
+    if(inputValue) {
+      var filteredData = tableData.filter((row) => row.datetime === inputValue);
 
-// if (tableData.inputValue.length !== 0) {
-//   populate(filterData);
-// }
-//   else if (response.filterData.length === 0 && ((response.filterCity.length !== 0 || response.filterDate.length !== 0))){
-//     populate(filterCity) || populate(filterDate);
+    }
 
-//   }
-//   else {
-//     tbody.append("tr").append("td").text("No results found!"); 
-//   }
-// })
+    createTable(filteredData);
 
+}
 
-
-
-//   // // Select the button
-//   // var button = d3.select("#button");
-
-//   // // // Select the form
-//   // // var form = d3.select("li");
-
-//   // // Create event handlers 
-//   // button.on("click", runEnter);
-//   // // form.on("submit",runEnter);
-
-//   // // Complete the event handler function for the form
-//   // function runEnter() {
-
-//   //   // Prevent the page from refreshing
-//   //   d3.event.preventDefault();
-    
-//   //   // Select the input element and get the raw HTML node
-//   //   var inputElement = d3.select("#datetime");
-
-//   //   // Get the value property of the input element
-//   //   var inputValue = inputElement.property("value");
-
-//   //   console.log(inputValue);
-//   //   console.log(tableData);
-
-//   //   var filteredData = tableData.filter(person => person.bloodType == inputValue);
-
-//   //   console.log(filteredData);
+createTable(tableData);
